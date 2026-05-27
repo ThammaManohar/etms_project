7ec1e1ed8509beaf9561d35eab73460c3b265721
Thamma <venkata.sai.m.thamma@accenture.com>
2026-05-27 14:08:22 +0530

    Added SQLite connection and table creation

--- data/storage.py
+++ data/storage.py
@@ -1,2 +1,32 @@
-employees_db = {}
-tasks_db = {}
+import sqlite3
+
+
+def get_connection():
+    conn = sqlite3.connect("etms.db")
+    return conn
+
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS employees (
+        emp_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        task_id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        assigned_to INTEGER,
+        status TEXT,
+        FOREIGN KEY (assigned_to) REFERENCES employees(emp_id)
+    )
+    """)
+
+    conn.commit()
+    conn.close()
